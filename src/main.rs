85bd46a86b9d732d00a675e59e111999fab5f564
Kartofi <anatoli7707@gmail.com>
2024-05-27 22:17:16 +0300

    Testing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use choki::structs::*;
 use choki::*;
-use reqwest::{Request, Response};
+use reqwest;
 
 fn main() {
     let mut server = Server::new(Some(1024));
     server
-        .get(
-            "/".to_string(),
-            |mut req: choki::structs::Request, mut res: choki::structs::Response| {
-                res.send_string("Hello");
-            },
-        )
+        .get("/".to_string(), |mut req: Request, mut res: Response| {
+            let body = reqwest::blocking::get("https://www.rust-lang.org")
+                .unwrap()
+                .text()
+                .unwrap();
+            res.send_bytes(&body.as_bytes(), None);
+        })
         .unwrap();
     server.listen(3000, None).unwrap();
     Server::lock();
